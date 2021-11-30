38cca04c708323e78928a7891d4a71059f24708f
molynatep <71908029+molynatep@users.noreply.github.com>
2021-11-30 04:26:13 -0500

    yay overflow is fixed

--- show.js
+++ show.js
@@ -15,12 +15,3 @@ document.getElementById('wrapper2').onclick = function() {
                          className.replace(' active ', ' ') :
                          this.className + ' active';
 }
-
-document.getElementById('wrapper3').onclick = function() {
-
-    var className = ' ' + wrapper3.className + ' ';
-
-    this.className = ~className.indexOf(' active ') ?
-                         className.replace(' active ', ' ') :
-                         this.className + ' active';
-}
