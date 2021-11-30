16f859e309fe63a04646fb48b719f3715ef17f79
molynatep <71908029+molynatep@users.noreply.github.com>
2021-11-30 02:02:43 -0500

    arrow stuff

--- final/build/script.js
+++ final/build/script.js
@@ -93,3 +93,32 @@ controlsContainer.onclick = function(e){
   activeImage.setAttribute('data-active', 'active');
   }  
 }
+
+
+//PHOTOS AND REVIEW
+document.getElementById('wrapper').onclick = function() {
+
+   var className = ' ' + wrapper.className + ' ';
+
+   this.className = ~className.indexOf(' active ') ?
+                        className.replace(' active ', ' ') :
+                        this.className + ' active';
+};
+
+document.getElementById('wrapper2').onclick = function() {
+
+   var className = ' ' + wrapper2.className + ' ';
+
+   this.className = ~className.indexOf(' active ') ?
+                        className.replace(' active ', ' ') :
+                        this.className + ' active';
+}
+
+document.getElementById('wrapper3').onclick = function() {
+
+   var className = ' ' + wrapper3.className + ' ';
+
+   this.className = ~className.indexOf(' active ') ?
+                        className.replace(' active ', ' ') :
+                        this.className + ' active';
+}
